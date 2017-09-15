fcf9f52b910e2904454a0e022829424e66b37fe6
Juan David Lopez Londoño <juandavidlopez95@yahoo.com>
2017-09-15 11:45:26 -0700

    Added loading spinner to the search pop-up on the Staff portal page. The load spinner activates when a user begins to type inside the input box.

---
+++ js/search.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+    console.log("Window has finished loading!");
+
+    var searchInputBox = document.getElementById("search-field");
+    var searchInput_value = searchInputBox.value;
+
+    var search_modal_load_indicator = document.getElementById("search-modal-load-indicator");
+
+    searchInputBox.addEventListener("keyup", function() {
+        search_modal_load_indicator.style.visibility = searchInputBox.value.length ? 'visible' : 'hidden';
+        console.log("Key pressed on input box");
+    }, false);
+
+}; //end window.onload() function
+
+console.log("JavaScript file loaded");
