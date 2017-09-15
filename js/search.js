1e985cfd033af66476e5cf08f33bab5153f29be2
Juan David Lopez Londoño <juandavidlopez95@yahoo.com>
2017-09-15 12:11:07 -0700

    Added event listener for hiding the loading spinner when the search input box loses focus

--- js/search.js
+++ js/search.js
@@ -7,8 +7,13 @@ window.onload = function() {
     var search_modal_load_indicator = document.getElementById("search-modal-load-indicator");
 
     searchInputBox.addEventListener("keyup", function() {
-        search_modal_load_indicator.style.visibility = searchInputBox.value.length ? 'visible' : 'hidden';
+        search_modal_load_indicator.style.visibility = searchInputBox.value.length ? "visible" : "hidden";
         console.log("Key pressed on input box");
+    }, false);
+
+
+    searchInputBox.addEventListener("blur", function() {
+        search_modal_load_indicator.style.visibility = "hidden";
     }, false);
 
 }; //end window.onload() function
