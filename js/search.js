029c3c133f48405e340c02e3064723893bed0667
Juan David Lopez Londoño <juandavidlopez95@yahoo.com>
2017-09-29 10:13:37 -0700

    Updated the search function to work and output buttons that match the search query (although the text search algorithm itself needs to be vastly improved)!  However, upon emptying the search textbox, and inputting a query again, duplicate buttons are generated.

--- js/search.js
+++ js/search.js
@@ -20,45 +20,94 @@ window.addEventListener("load", function() {
     // console.log(typeof getElementsByClassName('buttonLink'));
 
     function Search() {
+        var searchResults_buttonRow = document.getElementById("searchResultsButtonRow");
+        var searchResults_blueSection = document.getElementById("searchResults-blueSection");
         var searchInputBox = document.getElementById("search-field");
         var searchInput_value = searchInputBox.value;
+        var searchResults = document.getElementsByClassName("search-result");
 
-        if (searchInput_value == '' || null || undefined) {
-            searchResults_blueSection.style.display = "none";
-        }
-        //var buttonLinks = getElementsByClassName('buttonLink');
-        // console.log(getElementsByClassName('buttonLink').item(1));
-        //for (var i = 0; i < buttonLinks.length; i++) {
-        search_inputValue(document.getElementById('sampleLink'));
-        console.log("For loop");
-        // }
+        if (searchInput_value == '' || searchInput_value == null || searchInput_value == undefined) {
+            if (searchResults[0] !== undefined || searchResults[0] !== null) {
+                function removeElementsByClass(className) {
+                    var classElements = document.getElementsByClassName(className);
+                    while (classElements.length > 0) {
+                        classElements[0].parentNode.removeChild(classElements[0]);
+                    } //end while loop
+                    searchResults_blueSection.style.display = "none";
+                } //end removeElementsByClass()
+            } //end inner if-statement
+            //else { searchResults_blueSection.style.display = "none"; }
+        } //remove external if-statement
+
+        var buttonLinkElements = document.getElementsByClassName('buttonLink');
+        console.log("item(1): " + buttonLinkElements.item(1));
+        for (var i = 0; i < buttonLinkElements.length; i++) {
+            //search_inputValue(document.getElementById('sampleLink'));
+            console.log("Current Button Link ID: ", buttonLinkElements[i].id);
+            console.log("For loop");
+
+            if (buttonLinkElements[i] !== null || buttonLinkElements[i] !== undefined) {
+                console.log("Pass to search_inputValue()");
+                //search_inputValue(document.getElementById(buttonLinkElements[i].id));
+                search_inputValue(buttonLinkElements[i]);
+            }
+        } //end for loop
 
         function search_inputValue(linkElement) {
-            if (linkElement.getAttribute('data-title') == searchInput_value) {
+            console.log("Inside search_inputValue() \n\n");
+            if ((linkElement != null) && (linkElement.getAttribute('data-title').indexOf(searchInput_value) > -1)) {
+                console.log("search_inputValue() if-statement\n\n");
                 renderSearchResults(linkElement);
             }
         }
 
         function renderSearchResults(linkElement) {
-            console.log("Render search results");
-            var searchResults_buttonRow = document.getElementById("searchResultsButtonRow");
-            var searchResults_blueSection = document.getElementById("searchResults-blueSection");
-            var link_element = document.createElement("a");
-            link_element.href = linkElement.href;
+            console.log("renderSearchResults() function " + searchResults[0]);
+            console.log("searchResults Type: " + searchResults + "\n\n");
 
-            var button_element = document.createElement("button");
+            if (searchResults[0] === undefined) {
+                console.log("If-statement Render search results");
+                var link_element = document.createElement("a");
+                link_element.href = linkElement.href;
+                link_element.classList.add("search-result");
 
-            var image_element = document.createElement("img");
+                var button_element = document.createElement("button");
 
-            console.log(linkElement.firstChild.firstChild.src);
+                var image_element = document.createElement("img");
 
-            image_element.src = linkElement.firstChild.firstChild.src;
+                console.log(linkElement.firstChild.firstChild.src);
 
+                image_element.src = linkElement.firstChild.firstChild.src;
 
-            searchResults_buttonRow.appendChild(link_element).appendChild(button_element).appendChild(image_element);
-            searchResults_blueSection.style.display = "block";
-        }
+                searchResults_buttonRow.appendChild(link_element).appendChild(button_element).appendChild(image_element);
+                searchResults_blueSection.style.display = "block";
+            } //end if statement
+            else if (searchResults !== undefined) {
+                for (var i = 0; i < searchResults.length; i++) {
+                    //If statement to check if the actual link of link element does not already exist
+                    if ((searchResults[i] != undefined) && (searchResults[i].href != linkElement.href)) {
+                        console.log("Render search results, more buttons");
 
+                        console.log("Search Result link: " + searchResults[i].href);
+                        console.log("LinkElement " + linkElement.href);
+
+                        var link_element = document.createElement("a");
+                        link_element.href = linkElement.href;
+                        link_element.classList.add("search-result");
+
+                        var button_element = document.createElement("button");
+
+                        var image_element = document.createElement("img");
+
+                        console.log(linkElement.firstChild.firstChild.src);
+
+                        image_element.src = linkElement.firstChild.firstChild.src;
+
+                        searchResults_buttonRow.appendChild(link_element).appendChild(button_element).appendChild(image_element);
+                    } //end if-statement
+                } //end for loop
+            } //end else-statement
+        } //end renderSearchResults() function
     }
 
     searchInputBox.addEventListener('keypress', Search);
@@ -68,9 +117,9 @@ window.addEventListener("load", function() {
     //Search();
 
     /*Array.prototype.forEach.call(function(element) {
-    
+        
                 }, this); */
 
-}); //end window.onload() function ) 
+}); //end window.onload() function 
 
 console.log("JavaScript file loaded");
