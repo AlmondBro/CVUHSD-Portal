630a6116ce4d0277d74c79ac17475f92849bbe04
JuanDavidLopez95 <juandavid@jdlondono.com>
2020-04-28 12:04:49 -0700

    Create iframe styled components to match the system statuses and all links iframe

--- src/components/BlueSection/BlueSection.js
+++ src/components/BlueSection/BlueSection.js
@@ -6,6 +6,20 @@ import BlueSectionButton from "./BlueSectionButton.js";
 //Import 3rd-party APIs
 import styled from 'styled-components';
 
+
+let AllLinksGoogleSheet = styled("iframe")`
+    width: 25%;
+    height: 90vh;
+`; //end AllLinksGoogleSheet
+
+let SystemStatuses = styled("iframe")`
+    align: "center";
+    height: 400px;
+    width: 1200px;
+    border: 0;
+`; //end SystemStatuses
+
+
 const BlueSection = (props) => {
     let generateBlueSectionButtons = () => {
         console.log("generateBlueSectionButtons()");
@@ -48,22 +62,17 @@ const BlueSection = (props) => {
             return buttonsArray; */
         
         } else if (props.buttonRowID === "systemStatusesButtonRow") {
-           return  (<iframe title="System Statuses"
-                            style={ {   align: "center",
-                                        height: "400px",
-                                        width: "1200px",
-                                        border: "0", 
-                                        frameborder: "0"}
-                                } 
-                src='https://www.site24x7.com/sv.do?id=-lTskTIBFC99AjBdJTzdd22ylcZvGBYnfGhcgwvt1-27W89lFFvf7WICSx8TdzUT6kB92hYLWdGYIInKaxcmHcJTzDPBf7IFLjpWmnUEJ18%3D&st=false' scrolling='yes'></iframe>);
-           // return <BlueSectionButton />
+           return  (<SystemStatuses 
+                        title="System Statuses"  
+                        src='https://www.site24x7.com/sv.do?id=-lTskTIBFC99AjBdJTzdd22ylcZvGBYnfGhcgwvt1-27W89lFFvf7WICSx8TdzUT6kB92hYLWdGYIInKaxcmHcJTzDPBf7IFLjpWmnUEJ18%3D&st=false' 
+                        scrolling='yes'
+                    />);
         } else if (props.buttonRowID === "webAdminButtonRow") {
             return (
-                    <iframe title="All Links Google Sheets"
+                    <AllLinksGoogleSheet title="All Links Google Sheets"
                             className="google-sheet" 
                             src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSOPMzFTLmTXsOzY172KN_3IaJqeO9bLPl_3TIgc_bBQiWEanznykV6cEiPBuV9WUHEnL2vesphHEWZ/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false"
-                    >
-                    </iframe>);
+                    />);
         }//end else-statement
     }; //end generateBlueSectionButtons()
 
