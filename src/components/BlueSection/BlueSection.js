6f653060d37caada480622ca766ff9f56b7a9bbc
JuanDavidLopez95 <juandavid@jdlondono.com>
2020-06-09 11:23:20 -0700

    Prettify the BlueSection.js file code.

--- src/components/BlueSection/BlueSection.js
+++ src/components/BlueSection/BlueSection.js
@@ -9,61 +9,41 @@ const BlueSection = (props) => {
     let generateBlueSectionButtons = () => {
         console.log("generateBlueSectionButtons()");
 
-        if ( (typeof(props.buttons) !== "undefined" || props.buttons != null) && props.buttonRowID !== "systemStatusesButtonRow") {
+        if ( (  (typeof(props.buttons) !== "undefined") || (props.buttons != null) )
+                && (props.buttonRowID !== "systemStatusesButtonRow")
+            ) {
             
-            let buttonsArray = Object.values(props.buttons);
-            
-            return [...buttonsArray].map( (buttonObject, index) => {
-               /* Group buttons in tabs of four */
-              /*  if (index %4 === 0 && index >= 4 ) {
-                    return (
-                        <div key={index}></div>
-                        ); 
-
-                    return null;
-                } //end if statement */
-
-                return (<BlueSectionButton 
-                            key={index} 
-                            buttonLink={buttonObject.buttonLink} 
-                            buttonImg={buttonObject.buttonImg} 
-                            description={buttonObject.description}
-                        />); 
+                let buttonsArray = Object.values(props.buttons);
+                
+                return [...buttonsArray].map( (buttonObject, index) => {
+                    return (<BlueSectionButton 
+                                key         =   { index } 
+                                buttonLink  =   { buttonObject.buttonLink} 
+                                buttonImg   =   { buttonObject.buttonImg } 
+                                description =   { buttonObject.description }
+                            />); //end inner return statement
             }); //return map() function
 
             /* For loop can work here, but it does not return any new values whereas map does: 
                 Source: https://stackoverflow.com/questions/45576223/why-are-for-loops-not-allowed-in-react-jsx
             */
-           /*
-            let buttonsArray = [];
-            for (let i=0; i < props.buttons.length; i++) {
-                buttonsArray.push(<BlueSectionButton 
-                                    key={i} 
-                                    buttonLink={props.buttons.buttonLink} 
-                                    buttonImg={props.buttons.buttonImg} 
-                                />);
-            }
-            
-            return buttonsArray; */
-        
         } else if (props.buttonRowID === "systemStatusesButtonRow") {
            return  (<SystemStatuses 
-                        title="System Statuses"  
-                        src='https://www.site24x7.com/sv.do?id=-lTskTIBFC99AjBdJTzdd22ylcZvGBYnfGhcgwvt1-27W89lFFvf7WICSx8TdzUT6kB92hYLWdGYIInKaxcmHcJTzDPBf7IFLjpWmnUEJ18%3D&st=false' 
-                        scrolling='yes'
+                        title       = "System Statuses"  
+                        src         = 'https://www.site24x7.com/sv.do?id=-lTskTIBFC99AjBdJTzdd22ylcZvGBYnfGhcgwvt1-27W89lFFvf7WICSx8TdzUT6kB92hYLWdGYIInKaxcmHcJTzDPBf7IFLjpWmnUEJ18%3D&st=false' 
+                        scrolling   = 'yes'
                     />);
         } else if (props.buttonRowID === "webAdminButtonRow") {
             return (
-                    <AllLinksGoogleSheet title="All Links Google Sheets"
-                            className="google-sheet" 
-                            src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSOPMzFTLmTXsOzY172KN_3IaJqeO9bLPl_3TIgc_bBQiWEanznykV6cEiPBuV9WUHEnL2vesphHEWZ/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false"
-                            scolling="yes"
+                    <AllLinksGoogleSheet 
+                            title       = "All Links Google Sheets"
+                            className   = "google-sheet" 
+                            src         = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSOPMzFTLmTXsOzY172KN_3IaJqeO9bLPl_3TIgc_bBQiWEanznykV6cEiPBuV9WUHEnL2vesphHEWZ/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false"
+                            scolling    = "yes"
                     />
                     );
         }//end else-statement
     }; //end generateBlueSectionButtons()
-
-    
 
     /* 
         Interesting piece of information about the checked property in React: 
@@ -74,41 +54,44 @@ const BlueSection = (props) => {
     */
 
     return (
-        <BlueSectionContainer   className="blue-section" 
-                                id={props.blueSectionName + "-blueSection"} 
-                                renderAsStudent={props.renderAsStudent}
+        <BlueSectionContainer   className       =   "blue-section" 
+                                id              =   { props.blueSectionName + "-blueSection" } 
+                                renderAsStudent =   { props.renderAsStudent }
         >
             <InputCheckBoxHack 
-                type="checkbox" 
-                className="checkbox-hack blueSection-collapseToggle" 
-                id= {props.blueSectionName + "-collapseToggle"} 
-                defaultChecked={props.expanded} 
-                renderAsStudent={props.renderAsStudent}
+                type            =   "checkbox" 
+                className       =   "checkbox-hack blueSection-collapseToggle" 
+                id              =   { props.blueSectionName + "-collapseToggle" } 
+                defaultChecked  =   { props.expanded } 
+                renderAsStudent =   { props.renderAsStudent }
             />
             <SectionHeader 
-                className="section-header" 
-                title={props.title}
-                renderAsStudent={props.renderAsStudent}
+                className       =   "section-header" 
+                title           =   { props.title }
+                renderAsStudent =   { props.renderAsStudent }
             >
                 <h3>{props.headerTitle}</h3> 
-                <label htmlFor={props.blueSectionName + "-collapseToggle"}>
+                <label 
+                    htmlFor =   { props.blueSectionName + "-collapseToggle" }
+                >
                     <OpenColumnButton   
-                        className="open-column-button"  
-                        title={props.title}
-                        renderAsStudent={props.renderAsStudent}
+                        className       =   "open-column-button"  
+                        title           =   { props.title }
+                        renderAsStudent =   { props.renderAsStudent }
                     >
                         
                     </OpenColumnButton>
                 </label>
             </SectionHeader>
-            <ButtonRow  className="row button-row" 
-                        id={props.buttonRowID} 
-                        title={props.title}
-                        renderAsStudent={props.renderAsStudent}
+            <ButtonRow  
+                        className       =  "row button-row" 
+                        id              =   { props.buttonRowID } 
+                        title           =   { props.title }
+                        renderAsStudent =   { props.renderAsStudent }
             >
-            {
-                generateBlueSectionButtons()
-            }
+                {
+                    generateBlueSectionButtons()
+                }
             </ButtonRow>
         </BlueSectionContainer>
     );
