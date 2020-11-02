041620b1ff06bcd1a3321893140e8f609336d94a
AlmondBro <juandavid@jdlondono.com>
2020-11-02 15:08:52 -0800

    Destructure the req.body in the create request for the helpdesk router

--- server/routes/helpdesk/helpdesk-routes.js
+++ server/routes/helpdesk/helpdesk-routes.js
@@ -4,7 +4,15 @@ import isDev from 'isdev';
 const router = Router();
 
 router.post('/request/create', (req, res) => {
-
+    let {     
+        supportRequestTitle,
+        category,
+        description,
+        location,
+        phoneExt,
+        room,
+        attachment 
+    } = req.body;
 });
 
 module.exports = router;
