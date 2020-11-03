6262b707a1d3a85abcf557b9b05b32977b0207ed
AlmondBro <juandavid@jdlondono.com>
2020-11-03 07:32:47 -0800

    Get the create-request route working on postman.

--- server/routes/helpdesk/helpdesk-routes.js
+++ server/routes/helpdesk/helpdesk-routes.js
@@ -1,9 +1,59 @@
 import { Router } from 'express';
 import isDev from 'isdev';
 
+import fetch from 'node-fetch';
+
 const router = Router();
 
-router.post('/request/create', (req, res) => {
+const createSDPRequest = async ()  => {
+    /* //Headers:
+    'Content-Type': 'application/json',
+        'credentials': 'include',
+        'Access-Control-Allow-Origin': '*',
+    */
+    const sdpCreateRequestsURL = `${process.env.SDP_URL}/api/v3/requests`; 
+
+    const sdpCreateRequestsURLHeaders = {
+        "technician_key"    :   process.env.SDP_TECH_KEY
+    };
+
+    let sdpCreateReqResponse = await fetch(sdpCreateRequestsURL, {
+        method: 'POST',
+        headers: sdpCreateRequestsURLHeaders,
+        body: JSON.stringify({
+            request: {
+                subject: "Test Ticket",
+                description: "Test Ticket",
+                requester: {
+                    id: "4",
+                    name: "administrator",
+                    email_id: "lopezj@centinela.k12.ca.us"
+                },
+                status: {
+                    name: "Open"
+                }
+            }
+        })
+    })
+    .then((serverResponse) => {
+
+        console.log("\n", serverResponse);
+        return serverResponse.json(); //Parse the JSON of the response
+    })
+    .then((response) => {
+        console.log("response JSON:\t", response);
+        return response;
+    })
+    .then(serverResponse => serverResponse )
+    .catch((error) => {
+        toastNotify(error);
+        console.error(`Catching error:\t ${error}`);
+    });
+
+    return sdpCreateReqResponse;
+};
+
+router.post('/request/create', async (req, res) => {
     let {     
         supportRequestTitle,
         category,
@@ -13,6 +63,10 @@ router.post('/request/create', (req, res) => {
         room,
         attachment 
     } = req.body;
+
+
+    const sdpCreateReqResponse = await createSDPRequest();
+    return res.json({ ...sdpCreateReqResponse });
 });
 
 module.exports = router;
