5d4ec934dc5fd650ba56d40ec2f9ae3599a76313
AlmondBro <juandavid@jdlondono.com>
2020-12-15 14:52:50 -0800

    Use toLowerCase() method in the footer.

--- src/components/Footer/Footer_StyledComponents.js
+++ src/components/Footer/Footer_StyledComponents.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 let FooterStyled = styled("footer")`
     width: 100%;
     background-color: ${ props => props.title ?
-                                    ( (props.title === "student") || props.renderAsStudent === true || window.location.pathname === "/student") ? 
+                                    ( (props.title.toLowerCase() === "student") || props.renderAsStudent === true || window.location.pathname === "/student") ? 
                                         "#931E1D": "#1E6C93"
                                     : "#931E1D" 
                         };
