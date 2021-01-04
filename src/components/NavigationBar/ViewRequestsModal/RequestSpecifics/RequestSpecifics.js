c3521a8d728262fa493b2f33fa6fc1d7e29a8fc8
= <juandavid@jdlondono.com>
2021-01-03 23:03:52 -0800

    Begin to shape out the request specifics component.

--- src/components/NavigationBar/ViewRequestsModal/RequestSpecifics/RequestSpecifics.js
+++ src/components/NavigationBar/ViewRequestsModal/RequestSpecifics/RequestSpecifics.js
@@ -1,16 +1,147 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
-import { Container } from './RequestSpecificsStyledComponents.js';
+import Skeleton from 'react-loading-skeleton';
+import { faTasks, faCircle, faCheck, faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons';
+
+import undefsafe from 'undefsafe';
+
+//import styled components
+import { Container, Divider, Content, FAIconStyled, SubSection, IconSubSection, TimeDateSubSection, RequestTitle, RequestDescription, DateTime, TicketTypeCircleSkeleton } from './RequestSpecificsStyledComponents.js';
 
 const RequestSpecifics = ({districtPosition, renderAsStudent}) => {
     const { id } = useParams();
 
+    const status = "Open";
+    let subject, description, time, date = "test";
+
+    const getFAIcon = () => {
+        let faIcon;
+
+        switch(status) {
+            case "All":
+                faIcon = faTasks;
+            break;
+
+            case "Open":
+                faIcon = faCircle;
+            break;
+
+            case "In Progress":
+                faIcon = faAngleDoubleRight;
+            break;
+
+            case "Closed":
+                faIcon = faCheck;
+            break;
+        }
+
+        return faIcon;
+    };
+
+    const isLoading = false;
+
     return (
-        <Container>
-            <p>ID: { id }</p>
-        </Container>
-    );
+        <Container 
+            className   =   "request-rectangle-container" 
+            // onClick     =   { onClick }
+        >
+            {/* <Divider
+                className           =   "request-rectangle-divider"
+                districtPosition    =   { districtPosition }
+            /> */}
+
+            <Content className="request-rectangle-content">
+                <IconSubSection 
+                    className   =   "request-rectangle-subsection-icon"
+                    width       =   "6%"
+                    alignItems  =   "flex-start"
+                >
+                    {
+                        isLoading ? (
+                            <TicketTypeCircleSkeleton
+                                circle  =   { true }
+                                width   =   { 20  }
+                                height  =   { 20 }
+                            />
+                        ) : (
+                            <FAIconStyled
+                                className           =   "view-request-type-icon"
+                                districtPosition    =   { districtPosition.toLowerCase() }
+                                color               =   "white"
+                                icon                =   { getFAIcon() }
+                            />
+                        )
+                    }   
+                </IconSubSection>
+
+                <SubSection
+                    className   =   "request-rectangle-subsection"
+                    width       =   "53%"
+                    alignItems  =   "flex-start"
+
+                >
+                    <RequestTitle 
+                            className   =   "request-rectangle-req-title"
+                            districtPosition    =   { districtPosition.toLowerCase() }
+                    >
+                        {
+                            isLoading ? (
+                                <Skeleton
+                                    width = {300}
+                                />
+                            ) : undefsafe(subject, "") || "Request Subject"
+                        }  
+                    </RequestTitle>
+                    <RequestDescription
+                            className           =   "request-rectangle-description"
+                            districtPosition    =   { districtPosition.toLowerCase() }
+                    >
+                        {
+                            isLoading ? (
+                                <Skeleton
+                                    width = {200}
+                                />
+                            ) : undefsafe(description, "") || "I am working from home and do not have my contact info..."
+                        }  
+                    </RequestDescription>
+                </SubSection>
+
+                <TimeDateSubSection
+                    className       =   "request-rectangle-subsection-time-date"
+                    width           =   "35%"
+                    alignItems      =   "flex-end"
+                    as              =   "aside" 
+                >
+                    <DateTime
+                        className           =   "request-rectangle-date-time"
+                        districtPosition    =   { districtPosition.toLowerCase() }
+                        as                  =   "time"
+                    >
+                        {
+                            isLoading ? (
+                                <Skeleton
+                                    width = {40}
+                                />
+                            ) : undefsafe(time, "") || "12:51"
+                        }  
+                    </DateTime>
+                    <DateTime
+                        className           =   "request-rectangle-date-time"
+                        districtPosition    =   { districtPosition.toLowerCase() }
+                    >
+                        {
+                            isLoading ? (
+                                <Skeleton
+                                    width = {80}
+                                />
+                            ) : undefsafe(date, "") || "12/22/2020"
+                        }  
+                    </DateTime>
+                </TimeDateSubSection>
+            </Content>
+      </Container>  
+    ); //end return()
 }; //end RequestSpecifics()
 
 export default RequestSpecifics;
